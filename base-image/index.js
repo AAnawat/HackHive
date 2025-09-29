7a9d26c90bd0095f1b8eb9764c33251e1fb8db32
Anawat <66070217@kmitl.ac.th>
2025-09-29 23:50:55 +0700

    fix: modify base-image, optimize space

--- base-image/index.js
+++ base-image/index.js
@@ -1,45 +1,56 @@
 import { App } from "uWebSockets.js";
 import { Server } from "socket.io";
 import { spawn } from "node-pty";
-import os from 'os';
+import os from "os";
 
-
-const app = new App()
+const app = new App();
 const io = new Server({
-    cors: "*"
-})
-var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+    cors: "*",
+});
+var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 
-io.attachApp(app)
+io.attachApp(app);
 
 io.on("connection", (socket) => {
     console.log("Someone connected");
 
     const ptyProcess = spawn(shell, [], {
         name: "xterm-color",
-        env: process.env
-    })
+        env: {
+            ...process.env,
+            HOME: "/home/player",
+            USER: "player",
+            LOGNAME: "player",
+            SHELL: "/bin/bash",
+        },
+        cwd: "/home/player",
+        uid: 1000,
+        gid: 1000,
+    });
 
     socket.on("key", (packet) => {
         const data = JSON.parse(packet.toString());
 
         if (data.type === "onkey") {
-            ptyProcess.write(data.key)
+            ptyProcess.write(data.key);
         }
-    })
+    });
 
     ptyProcess.onData((data) => {
-        socket.emit("data", data)
-    })
+        socket.emit("data", data);
+    });
 
     socket.on("disconnect", () => {
-        ptyProcess.kill()
-    })
+        ptyProcess.kill();
+    });
+});
 
-})
+app.get("/health", (res, req) => {
+    res.writeStatus("200 OK").end(JSON.stringify({ status: "ok" }));
+});
 
 app.listen(1234, (token) => {
-  if (!token) {
-    console.warn("port already in use");
-  }
+    if (!token) {
+        console.warn("port already in use");
+    }
 });
