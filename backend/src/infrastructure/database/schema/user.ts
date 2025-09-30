f040b9f517f1c280a717459add16386720956ba6
Anawat <66070217@kmitl.ac.th>
2025-10-01 00:08:35 +0700

    fix: change default value in user pfp_path column

--- backend/src/infrastructure/database/schema/user.ts
+++ backend/src/infrastructure/database/schema/user.ts
@@ -1,13 +1,14 @@
 import { relations } from "drizzle-orm";
 import { bigserial, pgTable, varchar } from "drizzle-orm/pg-core";
 import { solvedRecordsTable } from "./solvedRecords";
+import profileManagerConfig from "../../../config/pictureManagerConfig";
 
 export const usersTable = pgTable("users", {
     id: bigserial({mode: 'number'}).primaryKey(),
     username: varchar({length: 255}).notNull(),
     gmail: varchar({length: 255}).notNull().unique(),
     password: varchar({length: 255}).notNull(),
-    pfp_path: varchar({length: 255}).notNull().default('/PFP/default.png'),  // TODO: chage when using S3 bucket
+    pfp_path: varchar({length: 255}).notNull().default(profileManagerConfig.defaultPath),  // TODO: chage when using S3 bucket
 })
 
 export const usersRelations = relations(usersTable, ({many}) => ({
